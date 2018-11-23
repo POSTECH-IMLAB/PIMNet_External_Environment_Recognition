619fb1d4102d6f5a73c4e071fac7f601978f572f
EunSeop <eunseop.lee90@gmail.com>
2018-11-23 21:45:06 +0900

    remove IR_animals.sh

--- test_IR_animals.sh
+++
@@ -1,3 +0,0 @@
-./darknet detector demo cfg/IR_animals.data cfg/IR_animals.cfg IR_animals_backup/IR_animals.backup deer_demo2.mp4
-# ./darknet detector test cfg/IR_animals.data cfg/IR_animals.cfg IR_animals_backup/IR_animals.backup 00004.jpg
-
--- train_IR_animals.sh
+++
@@ -1,4 +0,0 @@
-# Backup
-#./darknet detector train cfg/IR_animals.data cfg/IR_animals.cfg backup/IR_animals.backup -gpus 0,1
-# init_train
-./darknet detector train cfg/IR_animals.data cfg/IR_animals.cfg weights/darknet19_448.conv.23 -gpus 0,1
