d405ea03cf001f19c1e1b27809e002dd9ae61941
eunseop <eunseop.lee90@gmail.com>
2018-11-23 17:54:54 +0900

    Add IR based Animal Detector

---
+++ test_animals.sh
@@ -0,0 +1,4 @@
+# ./darknet detector demo cfg/animal.data cfg/animal.cfg backup_animal/yolo_final.weights deer_demo3.mp4 -i 0 
+
+./darknet detector test cfg/animal.data cfg/animal.cfg backup_animal/yolo_final.weights 00004.jpg
+
---
+++ train_animals.sh
@@ -0,0 +1,4 @@
+# Backup
+#./darknet detector train cfg/animals.data cfg/animals.cfg backup/animals.backup -gpus 0,1
+# init_train
+./darknet detector train cfg/animals.data cfg/animals.cfg weights/darknet19_448.conv.23 -gpus 0,1
