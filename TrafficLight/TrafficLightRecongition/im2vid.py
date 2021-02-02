5e022c0d92b67d3d344d0efcf06fa7f3b70e994e
postechjsk19 <jsk19@postech.ac.kr>
2021-02-02 13:15:56 +0900

    update

--- TrafficLight/TrafficLightRecongition/im2vid.py
+++ TrafficLight/TrafficLightRecongition/im2vid.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import os
 
-img_path = 'D:/project/LISA/images/test/result/'
+img_path = 'D:/project/LISA/images/test/'
 img_list = os.listdir(img_path)
 img_list = sorted(img_list,key=lambda x: int(os.path.splitext(x)[0]))
 img_array = []
@@ -17,7 +17,7 @@ for filename in img_list:
     img_array.append(img)
     print(filename)
 
-out = cv2.VideoWriter('D:/project/yolov5-master/TL.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 15, size)
+out = cv2.VideoWriter('D:/project/yolov5-master/demo.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 15, size)
 
 for i in range(len(img_array)):
     out.write(img_array[i])
