375cf6523bb6307a481d7c18b063c63174726955
jhseo <tohoaa@gmail.com>
2021-05-01 20:21:29 +0900

    add TSD

---
+++ config.py
@@ -0,0 +1,83 @@
+import os
+
+root = 'data'
+train_dir='train'
+test_dir='test'
+image_dir = os.path.join(root, train_dir)
+
+#annotation_file=os.path.join(root,'annotation.json')
+annotation_file = os.path.join(root, 'resnet50_predict.json')
+train_imageset_fn=os.listdir(image_dir)
+test_imageset_fn=os.listdir(os.path.join(root,test_dir))
+
+train_imageset_fn=list(map(lambda x:os.path.join(train_dir,x),train_imageset_fn))
+
+
+val_image_dir=os.path.join(root, test_dir)
+val_imageset_fn = list(map(lambda x:os.path.join(test_dir,x),test_imageset_fn))
+image_ext = '.jpg'
+
+backbone = 'resnet50'
+classes = ['pl',
+ 'il80',
+ 'pm',
+ 'pm30',
+ 'i2',
+ 'p12',
+ 'pl',
+ 'w57',
+ 'p26',
+ 'ph5',
+ 'pne',
+ 'pl',
+ 'pl',
+ 'pl',
+ 'pg',
+ 'i5',
+ 'p6',
+ 'p3',
+ 'pl',
+ 'pl20',
+ 'pl30',
+ 'pl40',
+ 'pl50',
+ 'pl60',
+ 'pl70',
+ 'pl80',
+ 'pl100',
+ 'pl120',
+ 'p27',
+ 'pm20',
+ 'pl',
+ 'ph4',
+ 'pm55',
+ 'il60',
+ 'p19',
+ 'pr40',
+ 'i4',
+ 'p23',
+ 'pn',
+ 'w32',
+ 'pl5',
+ 'p11',
+ 'p5',
+ 'ph4.5',
+ 'ip',
+ 'w13',
+ 'w59',
+ 'il100',
+ 'p10',
+ 'w55']
+mean, std = (0.499, 0.523, 0.532), (0.200, 0.202, 0.224)
+scale =None
+
+batch_size =8
+lr = 1e-4
+momentum = 0.9
+weight_decay =0.0002
+num_epochs =18
+lr_decay_epochs = [12]
+num_workers = 8
+width,height=512,512
+eval_while_training = True
+eval_every = 2
