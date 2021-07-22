601f4a8d1c67b80070008b84c2bb44a4a7768431
Taehun Kim <taehoon1018@postech.ac.kr>
2021-07-22 10:41:58 +0900

    Add files via upload

---
+++ Lane/utils/dataloader.py
@@ -0,0 +1,62 @@
+import os
+import cv2
+
+import numpy as np
+import torch.utils.data as data
+import torchvision.transforms as transforms
+
+from PIL import Image
+
+from utils.custom_transforms import *
+
+class RGB_Dataset(data.Dataset):
+    def __init__(self, root, transform_list):
+        image_root, gt_root = os.path.join(root, 'images'), os.path.join(root, 'masks')
+
+        self.images = [os.path.join(image_root, f) for f in os.listdir(image_root) if f.endswith('.jpg') or f.endswith('.png')]
+        self.images = sorted(self.images)
+        
+        self.gts = [os.path.join(gt_root, f) for f in os.listdir(gt_root) if f.endswith('.png')]
+        self.gts = sorted(self.gts)
+        
+        self.filter_files()
+        
+        self.size = len(self.images)
+        self.transform = self.get_transform(transform_list)
+
+    @staticmethod
+    def get_transform(transform_list):
+        tfs = []
+        for key, value in zip(transform_list.keys(), transform_list.values()):
+            if value is not None:
+                tf = eval(key)(**value)
+            else:
+                tf = eval(key)()
+            tfs.append(tf)
+        return transforms.Compose(tfs)
+
+    def __getitem__(self, index):
+        image = Image.open(self.images[index]).convert('RGB')
+        gt = Image.open(self.gts[index]).convert('L')
+        shape = gt.size[::-1]
+        name = self.images[index].split('/')[-1]
+        if name.endswith('.jpg'):
+            name = name.split('.jpg')[0] + '.png'
+            
+        sample = {'image': image, 'gt': gt, 'name': name, 'shape': shape}
+
+        sample = self.transform(sample)
+        return sample
+
+    def filter_files(self):
+        assert len(self.images) == len(self.gts)
+        images, gts = [], []
+        for img_path, gt_path in zip(self.images, self.gts):
+            img, gt = Image.open(img_path), Image.open(gt_path)
+            if img.size == gt.size:
+                images.append(img_path)
+                gts.append(gt_path)
+        self.images, self.gts = images, gts
+
+    def __len__(self):
+        return self.size
