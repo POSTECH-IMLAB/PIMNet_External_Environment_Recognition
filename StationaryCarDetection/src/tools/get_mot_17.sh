00e7d40add0a0ed3708f8b663d95437be461e210
cocoen12 <34021749+cocoen12@users.noreply.github.com>
2022-11-09 13:42:28 +0900

    Update get_mot_17.sh

--- StationaryCarDetection/src/tools/get_mot_17.sh
+++ StationaryCarDetection/src/tools/get_mot_17.sh
@@ -1,6 +1,9 @@
 mkdir ../../data/mot17
 cd ../../data/mot17
 wget https://motchallenge.net/data/MOT17.zip
+wget https://motchallenge.net/data/MOT20.zip
+
+
 unzip MOT17.zip
 rm MOT17.zip
 mkdir annotations
