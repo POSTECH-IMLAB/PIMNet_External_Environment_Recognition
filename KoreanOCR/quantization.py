f7e97106409662f76ce2938ae1a90073778dfbb1
hyunsk1995 <hyunskcj@gmail.com>
2022-11-09 20:07:08 +0900

    add quantization

---
+++ KoreanOCR/quantization.py
@@ -0,0 +1,23 @@
+import torch
+
+class QuantizedResNet18(nn.Module):
+    def __init__(self, model_fp32):
+        super(QuantizedResNet18, self).__init__()
+        # QuantStub converts tensors from floating point to quantized.
+        # This will only be used for inputs.
+        self.quant = torch.quantization.QuantStub()
+        # DeQuantStub converts tensors from quantized to floating point.
+        # This will only be used for outputs.
+        self.dequant = torch.quantization.DeQuantStub()
+        # FP32 model
+        self.model_fp32 = model_fp32
+
+    def forward(self, x):
+        # manually specify where tensors will be converted from floating
+        # point to quantized in the quantized model
+        x = self.quant(x)
+        x = self.model_fp32(x)
+        # manually specify where tensors will be converted from quantized
+        # to floating point in the quantized model
+        x = self.dequant(x)
+        return x
