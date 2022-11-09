c4beba4549193a395e02525078bc429469bc15ac
cocoen12 <34021749+cocoen12@users.noreply.github.com>
2022-11-09 13:43:48 +0900

    Update get_mot_17.sh

--- StationaryCarDetection/src/tools/get_mot_17.sh
+++ StationaryCarDetection/src/tools/get_mot_17.sh
@@ -7,12 +7,23 @@ wget https://motchallenge.net/data/MOT17.zip
 unzip MOT17.zip
 
 rm MOT17.zip
-rm MOT20.zip
+
 mkdir annotations
 cd ../../src/tools/
 python convert_mot_to_coco.py
 python convert_mot_det_to_results
 
 
+mkdir ../../data/mot20
+cd ../../data/mot20
 wget https://motchallenge.net/data/MOT20.zip
+
+
+
 unzip MOT20.zip
+
+rm MOT20.zip
+
+cd ../../src/tools/
+
+
